b03bc0480cd4e6bb2870c530718cfd913bca2079
JackPLP <90755235+JackPLP@users.noreply.github.com>
2022-01-13 19:40:30 +0100

    Geupdate naar het nieuwe model

--- code/headers/clift.h
+++ code/headers/clift.h
@@ -1,8 +1,7 @@
 #ifndef CLIFT_H
 #define CLIFT_H
-#include "cmotor.h"
 #include "cpushrot.h"
-#include "sensor.h"
+#include "cbelt.h"
 
 
 class cLift
@@ -10,14 +9,11 @@ class cLift
 public:
     cLift();
 public:
-    void activate(); //bij afgeven van blokje om lift omlaag te laten gaan
-    void checkDown(); //checken of de lift beneden is (zodat je de pushrod niet molt en ie ook echt wertkt)
-    void goUp(); //wanneer de lift beneden is pushrod activeren en omhoog gaan
-    bool isBlok; //of er een blokje op zit
+    void getBlock(int); //voor het krijgen van een blok van de vorige band, int voor welke het is
 private:
-    sensor sensor;
-   // cmotor motor;
+    cBelt liftBelt;
     cpushrot pushrod;
+    int liftPos; //status van de lift (omhoog/omlaag)
 };
 
 #endif // CLIFT_H
